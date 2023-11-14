b958f04364a3a4cd0f454ad832e02bdcecb6552e
Gbayi1 <133239898+Gbayi1@users.noreply.github.com>
2023-11-14 09:57:33 -0800

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,3 @@
--- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
-
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
-
+-- List top 3 cities
+-- Execute: cat 102-top_city.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
