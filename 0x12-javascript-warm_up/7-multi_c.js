2ebaa8e5c614c1a89b8f01227b19590d9eeea1df
Gbayi1 <133239898+Gbayi1@users.noreply.github.com>
2023-12-11 10:17:47 -0800

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 const { argv } = require('node:process');
 
 for(i=0; i<parseInt(argv[2]); i++){
-    console.log("C is fun");
+    console.log('C is fun');
 }
 
 if(!argv[2]){
-    console.log("Missing number of occurrences");
+    console.log('Missing number of occurrences');
 }
